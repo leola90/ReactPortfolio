f4109b96d729b1b9506802cfc2b5e67c61747373
Leo La <leola90@gmail.com>
2020-05-22 22:46:04 -0500

    modified App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,77 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./components/Navbar";
-// import { Layout, Header, Navigation, Drawer, Content } from  "react-mdl";
-import Contact from "./components/pages/contact";
-import Projects from "./components/pages/projects";
-import About from "./components/pages/about";
+import React, {Component} from "react";
+import BackDrop from "./components/BackDrop/Backdrop";
 import Home from "./components/pages/home";
+import Projects from "./components/pages/projects";
+import Contact from "./components/pages/contact";
+
+import Drawer from "./components/Drawer/Drawer"
+import LeftDrawer from "./components/LeftDrawer/LeftDrawer";
 
 
-function App() {
-  return (
-    <Router>
-    <div>
-      <NavBar />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/projects" component={Projects} />
-      <Route path="/contact" component={Contact} />
+
+class App extends Component{
+  //Set leftdrawer intially to false 
+  state = {
+    leftDrawerOpen: false,
+    currentPage: "Home"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  //Get previous state and then return object and update the state 
+  sideButtonToggleClickHandler = () => {
+    this.setState((prevState) => {
+    return { leftDrawerOpen: !prevState.leftDrawerOpen};
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({leftDrawerOpen: false});
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === "Home") {
+      return <Home />;
+    } else if (this.state.currentPage === "Projects") {
+      return <Projects />;
+    } else if (this.state.currentPage === "Contact") {
+      return <Contact />;
+    } else {
+      return <Home />;
+    }
+  };
+
+  render() {
+    let backdrop;
+  
+  
+    if (this.state.leftDrawerOpen) {
+      backdrop = <BackDrop onClickHandler={this.backdropClickHandler} />;
+    
+    }
+
+    return (
+      <div style={{height: "100%"}}>
+      <Drawer
+        currentPage={this.state.currentPage}
+        handlePageChange={this.handlePageChange}
+        sideButtonClickHandler={this.sideButtonToggleClickHandler} 
+      />
+      <LeftDrawer 
+      show={this.state.leftDrawerOpen}
+      handlePageChange={this.handlePageChange}
+      sideButtonClickHandler={this.sideButtonToggleClickHandler}
+      />
+      {backdrop}
+      {this.renderPage()}
     </div>
-  </Router>
+  
+    );
+  }
 
-  );
 }
 
 export default App;
+
